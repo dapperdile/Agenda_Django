4bc2023901140236c5810c9e79a2cd139dfb72b1
dapperdile <rafinhawc@hotmail.com>
2024-10-28 14:36:37 -0300

    Adicionado show e picturo ao model Contact, e criado pastas static e media

--- contact/admin.py
+++ contact/admin.py
@@ -9,5 +9,5 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'first_name', 'last_name'
+    list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
