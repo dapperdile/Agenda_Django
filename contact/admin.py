b0e38eecffe67843dc481e913f82ede84fdb4d0b
dapperdile <rafinhawc@hotmail.com>
2024-10-29 19:12:26 -0300

    Criado um codigo utilitário para gerar contatos aleatórios usando Faker

--- contact/admin.py
+++ contact/admin.py
@@ -5,7 +5,7 @@ from contact import models
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'id','first_name', 'last_name', 'phone',
-    ordering = 'id',
+    ordering = '-id',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
