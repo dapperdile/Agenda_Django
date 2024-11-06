4216c359a2cc3039ce479812a84e715482752df9
dapperdile <rafinhawc@hotmail.com>
2024-11-06 18:17:01 -0300

    Criado um partial para inserir paginação na pagina e procura

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.db.models import Q
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
     }
 
@@ -28,10 +33,14 @@ def search(request):
                 Q(last_name__icontains=search_value) |
                 Q(phone__icontains=search_value) |
                 Q(email__icontains=search_value))\
-        .order_by('-id')[:10]
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
     }
 
