6f22b3a0ca853462620a8b74d5d7219b35a8c6c3
dapperdile <rafinhawc@hotmail.com>
2024-11-12 19:02:44 -0300

    Criado definição para validar informações dos campos first_name e last_name

--- contact/forms.py
+++ contact/forms.py
@@ -26,12 +26,30 @@ class ContactForm(forms.ModelForm):
         # }
     
     def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error(
-            None,
-            ValidationError(
-                'Mensagem de Erro',
-                code='invalid'
-            )
-        )
+        if first_name == last_name:
+            msg = ValidationError(
+                    'O primeiro nome não pode ser igual ao sobrenome',
+                    code='invalid'
+                )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+            
         return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Mensagem de Erro do add_error',
+                    code='invalid'
+                )
+            )
+        
+        return first_name
